c4a69e92438bf67218321029d4c41a229b27da60
Hoodwinked53 <78771132+Hoodwinked53@users.noreply.github.com>
2021-07-01 19:10:05 -0500

    Through creating an Abstract Entity

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Skill description is required")
+    @Size(max = 600, message = "Skill description cannot exceed 600 characters")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
