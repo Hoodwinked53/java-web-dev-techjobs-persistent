2026a01718c2191c3fc325757671bd150e3d3faa
Hoodwinked53 <78771132+Hoodwinked53@users.noreply.github.com>
2021-07-09 09:26:20 -0500

    Finished Assignment

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,21 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany(mappedBy = "employer")
+    private List<Job> jobs = new ArrayList<>();
 
     @NotBlank(message = "Location is required")
     @Size(max = 50, message = "Location cannot exceed 50 characters")
     private String location;
 
-    public Employer(String location) {
-        this.location = location;
+    public Employer() {}
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 
-    public Employer() {}
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
     public String getLocation() {
         return location;
@@ -24,4 +35,5 @@ public class Employer extends AbstractEntity {
     public void setLocation(String location) {
         this.location = location;
     }
+
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,21 +1,30 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
 
     @NotBlank(message = "Skill description is required")
     @Size(max = 600, message = "Skill description cannot exceed 600 characters")
     private String description;
 
-    public Skill(String description) {
-        this.description = description;
+    public Skill() {}
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 
-    public Skill() {}
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
     public String getDescription() {
         return description;
